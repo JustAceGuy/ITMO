add5c2e5d4ec4e0dc5db48f7dd209891e39b184b
Dusty <dabondarenko7@gmail.com>
2025-09-19 20:31:34 +0300

    Update lab1.java
    cleaned up some useless code

--- Программирование/Лабораторные/lab 1/lab1.java
+++ Программирование/Лабораторные/lab 1/lab1.java
@@ -19,12 +19,8 @@ class lab1 {
                 w[i][j] = magic.arr3(i,j,e,x);
             }
         }
-
-        //System.out.println(arr2str.convert(e));
-        //System.out.println(arr2str.convert(x));
         for (int i = 0; i < 8; i++) {
-            System.out.println(arr2str.convert(w[i]));
-            System.out.println();
+            System.out.println(arr2str.convert(w[i]) + "\n");
         }
     }
 }
@@ -33,37 +29,25 @@ class magic{
     static double arr3(int i, int j, short[] e, double[] x_arr){
         double ret;
         double x = x_arr[j];
-        short[] condition_arr = {5, 7, 13, 15};
-        if (e[i] == 17) {
-            ret = x*(x+1);
-            ret = Math.pow(ret, 3);
-            ret = Math.log(Math.abs(Math.sin(ret)));
+        switch (e[i]) {
+            case 17:
+                ret = x*(x+1);
+                ret = Math.pow(ret, 3);
+                ret = Math.log(Math.abs(Math.sin(ret)));
+            break;
+            case 5, 7, 13, 15: 
+                ret = Math.pow(Math.E, Math.log(Math.abs(x)));
+                ret = Math.tan(ret);
+            break;
+            default:
+                double a = Math.log(Math.abs(x));
+                double b =  4/(Math.pow(x, x*(1-x))-0.5);
+                ret = Math.pow(a,b);
+                ret *= (0.25 + Math.cos(Math.asin( (x+6)/16 )));
+                ret = Math.pow(ret, 3);
+                //asin(sin) is pointless
         }
-        else if ( contains( condition_arr, e[i] ) ) {
-            ret = Math.pow(Math.E, Math.log(Math.abs(x)));
-            ret = Math.tan(ret);
-        }
-        else {
-            double a = Math.log(Math.abs(x));
-            double b =  4/(Math.pow(x, x*(1-x))-0.5);
-            double b_1 = Math.pow(x,x*(1-x));
-            //System.out.printf("%s, %s, %s | %s \n", a, b, b_1, x);
-            ret = Math.pow(a,b);
-            ret = ret * (0.25 + Math.cos(Math.asin( (x+6)/16 )));
-            ret = Math.pow(ret, 3);
-            //asin(sin) is pointless
-        }   
-        return ret;
-    }
-
-
-    static private boolean contains(short[] arr, int n) {
-        for (short i: arr) {
-            if (i == n) {
-                return true;
-            }
-        }
-        return false;
+        return ret; 
     }
 }
 
@@ -73,15 +57,7 @@ class arr2str{
         String ret = "";
         for (double i: arr) {
             if (!ret.isEmpty()) {ret = ret + "; ";}
-            ret = ret + String.format("%.3f", i);
-        }
-        return ret;
-    }
-    static String convert(short[] arr) {
-        String ret = "";
-        for (double i: arr) {
-            if (!ret.isEmpty()) {ret = ret + "; ";}
-            ret = ret + String.valueOf(i);
+            ret += String.format("%.3f", i);
         }
         return ret;
     }
